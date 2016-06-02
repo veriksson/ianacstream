cfe379519ba1b873665ba5be8f61e8b263b86cd4
Viktor Eriksson <v.eriksson@gmail.com>
2016-06-02 19:12:41 +0200

    More things

---
+++ HtmlGen/Html.fs
@@ -0,0 +1,99 @@
+﻿[<AutoOpen>]
+module Html
+
+open System.Text
+
+type Attr = 
+    | Style of (string * string) seq
+    | Id of string
+    | Class of string seq
+    | Type of string
+    | Src of string
+    | Action of string
+    | Method of string
+    | Rel of string
+    | Href of string
+
+type Node = 
+    | Document of Node seq
+    | Element of string * Attr seq * Node seq
+    | ClosedElement of string * Attr seq
+    | Text of string
+
+let renderAttrs (sb : StringBuilder) attrs = 
+    for attr in attrs do
+        match attr with
+        | Style(kvps) -> 
+            sb.Append(" style=\"") |> ignore
+            kvps |> Seq.iter (fun (k, v) -> sb.AppendFormat("{0}:{1};", k, v) |> ignore)
+            sb.Append("\"") |> ignore
+        | Id(id) -> sb.AppendFormat(" id=\"{0}\"", id) |> ignore
+        | Class(cls) -> sb.AppendFormat(" class=\"{0}\"", (String.concat " " cls)) |> ignore
+        | Type(typ) -> sb.AppendFormat(" type=\"{0}\"", typ) |> ignore
+        | Src(src) -> sb.AppendFormat(" src=\"{0}\"", src) |> ignore
+        | Action(act) -> sb.AppendFormat(" action=\"{0}\"", act) |> ignore
+        | Method(met) -> sb.AppendFormat(" method=\"{0}\"", met) |> ignore
+        | Rel(rel) -> sb.AppendFormat(" rel=\"{0}\"", rel) |> ignore
+        | Href(href) -> sb.AppendFormat(" href=\"{0}\"", href) |> ignore
+    sb
+
+let rec renderNode (sb : StringBuilder) = 
+    function 
+    | Document(children) -> 
+        sb.Append("<!DOCTYPE html>") |> ignore
+        sb.Append("<html>") |> ignore
+        for child in children do
+            renderNode sb child |> ignore
+        sb.Append("</html>") |> ignore
+        sb
+    | Element(name, attrs, children) -> 
+        sb.AppendFormat("<{0}", name) |> ignore
+        renderAttrs sb attrs |> ignore
+        sb.Append(">") |> ignore
+        for child in children do
+            renderNode sb child |> ignore
+        sb.AppendFormat("</{0}>", name)
+    | ClosedElement(name, attrs) -> 
+        sb.AppendFormat("<{0}", name) |> ignore
+        renderAttrs sb attrs |> ignore
+        sb.Append(" />")
+    | Text(s) -> sb.Append(s)
+
+let render node = (renderNode (StringBuilder()) node).ToString()
+let renderFn (nodeFn : unit -> Node) = render <| nodeFn()
+let elem name attrs children = Element(name, attrs, children)
+let elem' name attrs = ClosedElement(name, attrs)
+let text s = Text(s)
+let document children = Document(children)
+let body children = elem "body" [] children
+let head children = elem "head" [] children
+let title txt = elem "title" [] [ text txt ]
+let h1 attrs children = elem "h1" attrs children
+let h2 attrs children = elem "h2" attrs children
+let h3 attrs children = elem "h3" attrs children
+let h4 attrs children = elem "h4" attrs children
+let h5 attrs children = elem "h5" attrs children
+let table attrs children = elem "table" attrs children
+let th attrs children = elem "th" attrs children
+let tr attrs children = elem "tr" attrs children
+let td attrs children = elem "td" attrs children
+let div attrs children = elem "div" attrs children
+let p attrs children = elem "p" attrs children
+let hr attrs = elem' "hr" attrs
+let li attrs children = elem "li" attrs children
+let ul attrs children = elem "ul" attrs children
+let ol attrs children = elem "ol" attrs children
+let form attrs children = elem "form" attrs children
+let input attrs children = elem "input" attrs children
+let password (attrs : Attr seq) children = input (Seq.append [ Type "password" ] attrs) children
+let img attrs = elem' "img" attrs
+let script attrs code = elem "script" attrs [text code]
+let link attrs = elem' "link" attrs
+let br attrs = elem' "br" attrs
+let a attrs children = elem "a" attrs children
+
+(* html5 wank *)
+let header attrs children = elem "header" attrs children
+let nav attrs children = elem "nav" attrs children
+let section attrs children = elem "section" attrs children
+let footer attrs children = elem "footer" attrs children
---
+++ HtmlGen/Script.fsx
@@ -0,0 +1,29 @@
+﻿// Learn more about F# at http://fsharp.org. See the 'F# Tutorial' project
+// for more guidance on F# programming.
+// Define your library scripting code here
+module Script
+
+#load "Html.fs"
+
+open Html
+open System.Text
+open System.IO
+
+let cool name = 
+    div [ Class [ "avatar" ] ] [ img 
+                                     [ Src 
+                                           "https://lh3.googleusercontent.com/-TXoWkFG5oug/AAAAAAAAAAI/AAAAAAAAAAA/AMW9IgdtCOlWe0-48Ah5XNXibl_i2Vh7_A/s64-c-mo/photo.jpg" ]
+                                 p [] [ text (sprintf "User %d" name) ] 
+                                 hr [] ]
+
+let template () = 
+    document [ head []
+               body [ div [ Style [ "margin", "0 auto" ] ] [ h2 [] [ text "Welcome to my site" ]
+                                                             p [] [ text "This is a test" ]
+                                                             div [] [ for x in 1..12 -> cool x ] ] ] ]
+
+let test () = 
+    let render = Html.renderFn template
+    File.WriteAllText(@"C:\temp\test2.html", render)
+
+let test2() = Html.renderFn template
